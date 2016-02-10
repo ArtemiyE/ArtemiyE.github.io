a83d2643d24cd044d05be8bf2cfa0b81434bdead
ArtemiyE <aevdokiev@gmail.com>
2016-02-10 21:34:32 +0200

    синхронизировал репозиторий

--- karpol/js/script.js
+++ karpol/js/script.js
@@ -1,9 +1,31 @@
+$(function(){
+  $.scrollUp({
+   scrollName: 'scrollUp', // Element ID
+   scrollImg: true,
+   topDistance: '300', // Distance from top before showing element (px)
+   topSpeed: 300, // Speed back to top (ms)
+   animation: 'fade', // Fade, slide, none
+   animationInSpeed: 200, // Animation in speed (ms)
+   animationOutSpeed: 200, // Animation out speed (ms)
+   scrollText: 'Scroll to top', // Text for element
+   activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+  });
+});
+
 $(document).ready(function(){
   $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 3000,
+<<<<<<< 4e70577e0b198c5d6d6143852c8d6ecf8881ba30
     // adaptiveHeight: true,
     dots: true,
     // fade: true,
     // speed: 500
   }); });
+=======
+    fade: true,
+    speed: 300
+
+  });
+});
+>>>>>>> слайдер поломался, кнопка вверх работает
