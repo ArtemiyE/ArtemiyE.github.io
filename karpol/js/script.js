7d93bcab4745feafbba547df01eacfdc212ef7f3
ArtemiyE <aevdokiev@gmail.com>
2016-02-10 17:55:31 +0200

    Update script.js

--- karpol/js/script.js
+++ karpol/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('.slider').slick({
     autoplay: true,
-    // autoplaySpeed: 3000,
+    autoplaySpeed: 3000,
     // adaptiveHeight: true,
     dots: true,
-    fade: true,
-    speed: 500
+    // fade: true,
+    // speed: 500
   }); });
